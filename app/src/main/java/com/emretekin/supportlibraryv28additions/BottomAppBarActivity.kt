a5da269ac3f421dc856b8b58ff78feda10ac95da
emretekin <emretekin777@hotmail.com>
2018-09-12 10:00:55 +0300

    Activities created

---
+++ app/src/main/java/com/emretekin/supportlibraryv28additions/BottomAppBarActivity.kt
@@ -0,0 +1,12 @@
+package com.emretekin.supportlibraryv28additions
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BottomAppBarActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bottom_app_bar)
+    }
+}
---
+++ app/src/main/java/com/emretekin/supportlibraryv28additions/ChipGroupActivity.kt
@@ -0,0 +1,12 @@
+package com.emretekin.supportlibraryv28additions
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ChipGroupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chip_group)
+    }
+}
