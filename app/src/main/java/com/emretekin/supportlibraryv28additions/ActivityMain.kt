b767042c25395fc7fd0863f392eb42c6236f2970
emretekin <emretekin777@hotmail.com>
2018-09-17 02:24:22 +0300

    BottomAppBarActivity improvements

--- app/src/main/java/com/emretekin/supportlibraryv28additions/ActivityMain.kt
+++ app/src/main/java/com/emretekin/supportlibraryv28additions/ActivityMain.kt
@@ -9,12 +9,14 @@ import android.os.Bundle
 import android.support.design.button.MaterialButton
 import android.support.design.chip.Chip
 import android.support.v7.widget.AppCompatTextView
+import android.support.v7.widget.Toolbar
 import android.view.View
 import android.widget.ImageView
 import android.widget.Toast
 
 class ActivityMain : AppCompatActivity() {
 
+    private lateinit var toolbar: Toolbar;
     private lateinit var toolbarTitle: AppCompatTextView;
     private lateinit var arrowImageview: ImageView;
     private lateinit var bottomAppbar: MaterialButton
@@ -33,6 +35,7 @@ class ActivityMain : AppCompatActivity() {
     }
 
     private fun createViews(){
+        toolbar = findViewById(R.id.toolbar)
         toolbarTitle = findViewById(R.id.toolbar_title_textview)
         arrowImageview = findViewById(R.id.arrow_imageview)
         bottomAppbar = findViewById(R.id.sample_bottomAppbar_materialButton)
@@ -44,7 +47,7 @@ class ActivityMain : AppCompatActivity() {
 
     private fun setValues() {
         toolbarTitle.setText(getString(R.string.app_name))
-        arrowImageview.visibility = View.GONE;
+        toolbar.hideBackButton()
     }
 
     private fun setListeners(){
@@ -53,5 +56,13 @@ class ActivityMain : AppCompatActivity() {
         chip.setOnClickListener { startActivity(Intent(this, ChipActivity::class.java)) }
         chipGroup.setOnClickListener { startActivity(Intent(this, ChipGroupActivity::class.java)) }
         materialCardview.setOnClickListener { startActivity(Intent(this, MaterialCardviewActivity::class.java)) }
+    }
+
+    fun Toolbar.hideBackButton() {
+        arrowImageview.visibility = View.GONE;
+    }
+
+    private fun goToGithubPage(){
+        //https://github.com/emretekin/Support-Library-v28-Additions
     }
 }
--- app/src/main/java/com/emretekin/supportlibraryv28additions/BottomAppBarActivity.kt
+++ app/src/main/java/com/emretekin/supportlibraryv28additions/BottomAppBarActivity.kt
@@ -8,8 +8,11 @@ import android.support.design.bottomappbar.BottomAppBar
 import android.support.design.button.MaterialButton
 import android.support.design.widget.FloatingActionButton
 import android.support.v7.widget.AppCompatTextView
+import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_bottom_app_bar.*
+import kotlin.math.nextUp
 
 class BottomAppBarActivity : AppCompatActivity() {
 
@@ -18,6 +21,9 @@ class BottomAppBarActivity : AppCompatActivity() {
     private lateinit var arrowImageview: ImageView;
     private lateinit var fab: FloatingActionButton;
     private lateinit var fabAttachedButton: MaterialButton;
+    private lateinit var fabAlignmentButton: MaterialButton;
+    private lateinit var fabCradleVerticalOffsetButton: MaterialButton;
+    private lateinit var decFabCradleVerticalOffsetButton: MaterialButton;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,6 +40,9 @@ class BottomAppBarActivity : AppCompatActivity() {
         fab = findViewById(R.id.fab);
         toolbarTitle = findViewById(R.id.toolbar_title_textview);
         fabAttachedButton = findViewById(R.id.fabAttached_button);
+        fabAlignmentButton = findViewById(R.id.fabAlignment_button);
+        fabCradleVerticalOffsetButton = findViewById(R.id.fabCradleVerticalOffset_button);
+        decFabCradleVerticalOffsetButton = findViewById(R.id.dec_fabCradleVerticalOffset_button);
         arrowImageview = findViewById(R.id.arrow_imageview);
     }
 
@@ -43,16 +52,42 @@ class BottomAppBarActivity : AppCompatActivity() {
 
     private fun setListeners() {
         Handler().postDelayed({
-            bottomAppbar.setFabAttached(true)
+            // Anim
+            bottomAppbar.toggleFabAttached()
         }, 500)
+        Handler().postDelayed({
+            // Anim
+            bottomAppbar.toggleAlignment()
+        }, 750)
 
         arrowImageview.setOnClickListener { onBackPressed() }
-        fabAttachedButton.setOnClickListener {
-            if (bottomAppbar.isFabAttached) { bottomAppbar.setFabAttached(false) } else { bottomAppbar.setFabAttached(true) }
-        }
+        fabAttachedButton.setOnClickListener { bottomAppbar.toggleFabAttached() }
+        fabAlignmentButton.setOnClickListener { bottomAppbar.toggleAlignment() }
+        fabCradleVerticalOffsetButton.setOnClickListener {bottomAppbar.increaseCradleVerticalOffset(30) }
+        decFabCradleVerticalOffsetButton.setOnClickListener {bottomAppbar.increaseCradleVerticalOffset(0) }
+
+        fab.setOnClickListener { onBackPressed() }
     }
 
     private fun setBottomBarMenu() {
         bottomAppbar.replaceMenu(R.menu.bottom_appbar_menu)
+    }
+
+    //My Extension Function for BottomAppBar
+    fun BottomAppBar.toggleFabAttached() {
+        if (bottomAppbar.isFabAttached) {
+            bottomAppbar.setFabAttached(false)
+        } else {
+            bottomAppbar.setFabAttached(true)
+        }
+    }
+
+    fun BottomAppBar.toggleAlignment() {
+        val current = fabAlignmentMode
+        fabAlignmentMode = current.xor(1)
+    }
+
+    fun BottomAppBar.increaseCradleVerticalOffset(value: Int) {
+        bottomAppbar.setCradleVerticalOffset(value);
     }
 }
