029cd0025ae6ab56199ca816ce67944f993c45e8
emretekin <emretekin777@hotmail.com>
2018-10-23 13:13:07 +0300

    improvements on BottomAppBarDetailActivity

--- app/src/main/java/com/emretekin/supportlibraryv28additions/BottomAppBarDetailActivity.kt
+++ app/src/main/java/com/emretekin/supportlibraryv28additions/BottomAppBarDetailActivity.kt
@@ -1,15 +1,27 @@
 package com.emretekin.supportlibraryv28additions
 
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.support.design.bottomappbar.BottomAppBar
+import android.support.design.widget.FloatingActionButton
 import android.support.v7.widget.AppCompatTextView
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ImageView
 
 class BottomAppBarDetailActivity : AppCompatActivity() {
 
     private lateinit var toolbarTitle: AppCompatTextView;
     private lateinit var arrowImageview: ImageView;
+    private lateinit var bottomAppbar: BottomAppBar;
+    private lateinit var fab: FloatingActionButton;
+
+    private val GITHUB_URL = "https://github.com/emretekin/Support-Library-v28-Additions";
+    private val MEDIUM_URL = "https://medium.com/@emre.tekin/android-design-support-library-v28-3e0ea4d9f688";
+    private val MATERIAL_IO_URL = "https://material.io/develop/android/components/bottom-app-bar";
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +30,7 @@ class BottomAppBarDetailActivity : AppCompatActivity() {
         createViews()
         setValues()
         setListeners()
+        setBottomBarMenu()
     }
 
     private fun setValues() {
@@ -27,10 +40,38 @@ class BottomAppBarDetailActivity : AppCompatActivity() {
     private fun createViews() {
         toolbarTitle = findViewById(R.id.toolbar_title_textview);
         arrowImageview = findViewById(R.id.arrow_imageview);
+        bottomAppbar = findViewById(R.id.bottom_appbar);
+        fab = findViewById(R.id.fab);
     }
 
     private fun setListeners() {
         arrowImageview.setOnClickListener { onBackPressed() }
+    }
+
+    private fun setBottomBarMenu() {
+        bottomAppbar.replaceMenu(R.menu.bottom_appbar_menu)
+        setSupportActionBar(bottomAppbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.bottom_appbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item!!.itemId) {
+            R.id.github -> goToSomewhere(GITHUB_URL)
+            R.id.material -> goToSomewhere(MATERIAL_IO_URL)
+            R.id.about -> goToSomewhere(MEDIUM_URL)
+        }
+        return true
+    }
+
+    private fun goToSomewhere(url: String) {
+        val i = Intent(Intent.ACTION_VIEW)
+        i.data = Uri.parse(url)
+        startActivity(i)
     }
 }
 
